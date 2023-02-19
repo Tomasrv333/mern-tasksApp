46067cfae1bbca7113805b53e9b2f94df68e1105
Tomasrv333 <tomasrv123@gmail.com>
2023-02-19 14:46:14 -0500

    User route and controller created

--- Backend/src/routes/users.js
+++ Backend/src/routes/users.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { getUserInfo, updateUser } from '../controllers/user';
 
 const router = express.Router();
+
+router.get('/me/info', getUserInfo)
+router.put('/me', updateUser)
 
 export default router;
