5539822925e60676f1f634c1a87222d222e9e75b
Tomasrv333 <tomasrv123@gmail.com>
2023-02-22 13:47:51 -0500

    Login component created and connected

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import axios from 'axios';
 import classes from '../../Scss/AuthForm.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 
 const Login = () => {
+    const navigate = useNavigate()
+    const login = async (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+    
+        try{
+            await axios.post('/api/auth/login', {
+                email,
+                password,
+            });
+            navigate('/');
+            toast.success('Login Success')
+        } catch(err) {
+            console.log(err)
+            toast.error('Login Failed')
+        }
+    }
+
   return (
     <div className={classes.register}>
         <h1 className={classes.title}>
             Login
         </h1>
-        <form className={classes.authForm}>
+        <form className={classes.authForm} onSubmit={login}>
             <label htmlFor='email'>
                 Email
                 <input type='email' name='email' placeholder='email' required />
